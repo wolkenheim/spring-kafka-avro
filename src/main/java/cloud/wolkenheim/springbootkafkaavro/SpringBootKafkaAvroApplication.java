c51e159886da52ec71a53b481e644821042cc036
HCP <shopfreelancer@gmail.com>
2021-05-07 14:33:55 +0200

    initial commit: spring boot dependencies

---
+++ src/main/java/cloud/wolkenheim/springbootkafkaavro/SpringBootKafkaAvroApplication.java
@@ -0,0 +1,13 @@
+package cloud.wolkenheim.springbootkafkaavro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaAvroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaAvroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/wolkenheim/springbootkafkaavro/SpringBootKafkaAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.wolkenheim.springbootkafkaavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
